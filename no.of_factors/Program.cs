2c009da4d83510190bbf5094a94a505f4b9d6919
niyati salot <170120107148@git.org.in>
2020-12-29 11:49:24 +0530

    Add project files.

---
+++ no.of_factors/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace no.of_factors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a Number to find it's factors ! ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int cnt = 0;
+            for(int i = 1; i<num; i++)
+            {
+                if(num % i == 0)
+                {
+                    Console.WriteLine(i);
+                    cnt++;
+                   
+                }
+            }
+            Console.WriteLine("Total Factors = " + cnt);
+        }
+    }
+}
